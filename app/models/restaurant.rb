3f54235c810c5bb6dc7b0e6672d587fe89af85ed
Julien VENNIN <julien.vennin@gmail.com>
2019-08-15 10:56:19 +0200

    Ex yelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORIES = %W(chinese italian japanese french belgian)
+  CATEGORIES = %w(chinese italian japanese french belgian)
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
